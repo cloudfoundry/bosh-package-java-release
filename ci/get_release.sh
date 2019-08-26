4050a8f46f2945c682512d5de9048eadbc64f712
Huan Wang <huwang@pivotal.io>
2019-08-26 10:33:47 -0600

    add an improvement todo

--- ci/get_release.sh
+++ ci/get_release.sh
@@ -8,6 +8,7 @@ ADOPTOPENJDK_API='https://api.adoptopenjdk.net'
 
 releases="$(curl -Ss "${ADOPTOPENJDK_API}/v2/info/releases/openjdk${JAVA_VERSION}")"
 
+# TODO: improve using https://api.adoptopenjdk.net/#query-parameters directly instead of jq
 latest_linux_x64_releases="$(echo ${releases} | jq -r '.[] | select(.binaries[].os == "linux" and .binaries[].architecture == "x64") | .release_name' | grep -v 'openj9' | sort -rV | head -n1)"
 
 echo "Latest release of Java ${JAVA_VERSION} for linux is ${latest_linux_x64_releases}"
